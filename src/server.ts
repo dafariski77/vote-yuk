210edfe8ea6dedbde9b6ca5c81fc421ddb9c5647
dafariski77 <rskdafa@gmail.com>
2023-09-23 23:11:24 +0700

    add app name

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import notFoundMiddleware from "./middlewares/not-found";
 dotenv.config();
 
 const app = express();
+const appName = process.env.APP_NAME || "Vote Yuk";
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -19,4 +20,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`${appName} listening on port ${port}!`));
